f1ffe4784b05636e72740c4d8e553f6592efa5dd
Fluffy <fluffy.l2032@gmail.com>
2020-02-29 14:43:33 +0100

    'Release 3.9.499 [nolog]'

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.8.498")]
+[assembly: AssemblyFileVersion("3.9.499")]
