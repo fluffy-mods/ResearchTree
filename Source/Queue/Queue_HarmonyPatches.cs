1da67d18b9af3ff26f71ed117b165de326a41b5c
Zemogiter <zumi999@vp.pl>
2021-09-21 18:05:55 +0200

    Update Queue_HarmonyPatches.cs
    Prefix was spaming the in-game debug log with useless infromation. Removed it since it didn't do anything else.

--- Source/Queue/Queue_HarmonyPatches.cs
+++ Source/Queue/Queue_HarmonyPatches.cs
@@ -15,12 +15,6 @@ namespace FluffyResearchTree
             // check if last active project was finished. If so, try start the next project.
             // Thanks to NotFood for this nice simplification, I've adapted his/her code;
             // https://github.com/notfood/RimWorld-ResearchPal/blob/master/Source/Injectors/ResearchManagerPatch.cs
-            private static void Prefix( ResearchManager __instance, ref ResearchProjectDef __state )
-            {
-                __state = __instance.currentProj;
-                Log.Debug( "{0} progress: {1}", __state.LabelCap, __state.ProgressPercent );
-            }
-
             private static void Postfix( ResearchProjectDef __state )
             {
                 Log.Debug( "{0} finished?: {1}", __state, __state?.IsFinished );
